6e5084f2b4d02c33faa5761b7a7498be870e9cd6
John Ward <johnward127@gmail.com>
2024-09-06 17:25:40 +0100

    Add button to refresh the grid with the desired number of rows
    Refactor code to use functions to allow the refresh
    button to work as intended
    Add function to create divs, delete divs, and add
    event listener

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const gridContainer = document.querySelector("#grid-container");
-const userInput = prompt("How many rows?");
-const numberRows = parseInt(userInput);
-const numberDivs = numberRows * numberRows;
-document.documentElement.style.setProperty("--divs-per-row", numberRows);
+// const userInput = prompt("How many rows?");
+// const numberRows = parseInt(userInput);
+// const numberDivs = numberRows * numberRows;
+// document.documentElement.style.setProperty("--divs-per-row", numberRows);
 
-for (i = 0; i < numberDivs; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    gridContainer.appendChild(div);
+function addHoverEffectListener() {
+    const squareHover = document.querySelectorAll(".square");
+    squareHover.forEach((squareHoverEffect) => squareHoverEffect.addEventListener("mouseover", () => {
+        squareHoverEffect.classList.add("square-hover");
+    }))
+    return;
 }
 
-const squareHover = document.querySelectorAll(".square");
-squareHover.forEach((squareHoverEffect) => squareHoverEffect.addEventListener("mouseover",() => {
-    squareHoverEffect.classList.add("square-hover");
-}))
+function createDivs(numberProvidedByUser) {
+    for (i = 0; i < numberProvidedByUser * numberProvidedByUser; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        gridContainer.appendChild(div);
+    }
+    document.documentElement.style.setProperty("--divs-per-row", numberProvidedByUser);
+    addHoverEffectListener();
+    return;
+}
+
+function refreshGrid() {
+    const userInput = prompt("How many rows?");
+    const numberRows = parseInt(userInput);
+    const squareDelete = document.querySelectorAll(".square");
+    squareDelete.forEach((divDelete) => divDelete.remove())
+    createDivs(numberRows);
+    return;
+}
+createDivs(16);
