4375cf57a346cb4752c9815628910eb398042cde
John Ward <johnward127@gmail.com>
2024-09-06 17:01:59 +0100

    Add styling to the box to allow for variable numbers of columns and rows

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const gridContainer = document.querySelector("#grid-container");
-for (i = 0; i < 256; i++) {
+const userInput = prompt("How many rows?");
+const numberRows = parseInt(userInput);
+const numberDivs = numberRows * numberRows;
+document.documentElement.style.setProperty("--divs-per-row", numberRows);
+
+for (i = 0; i < numberDivs; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
     gridContainer.appendChild(div);
 }
 
 const squareHover = document.querySelectorAll(".square");
-squareHover.forEach((square) => square.addEventListener("mouseover",() => {
-    square.classList.add("square-hover");
+squareHover.forEach((squareHoverEffect) => squareHoverEffect.addEventListener("mouseover",() => {
+    squareHoverEffect.classList.add("square-hover");
 }))
